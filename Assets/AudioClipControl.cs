a8753ce62558dd7ea1293cdb65ace1d8c3683019
craigfahner <craig.fahner@gmail.com>
2024-03-05 19:42:23 -0500

    added a script that lets you fast forward and pause audio (useful for development)

---
+++ Assets/AudioClipControl.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioClipControl : MonoBehaviour
+{
+    public AudioSource theAudio;
+    public bool paused = false;
+    public int skipPosition = 0;
+    private int prevSkipPos = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (paused)
+        {
+            theAudio.Pause();
+        } else
+        {
+            theAudio.UnPause();
+        }
+
+        if(skipPosition != prevSkipPos)
+        {
+            theAudio.time = skipPosition;
+            prevSkipPos = skipPosition;
+        }
+    }
+}
